d50451949a5715f5210051f7872f36efcfdf44da
osipm <54916138+osipm@users.noreply.github.com>
2021-09-27 19:53:43 +0300

    виконав основне завдання

---
+++ src/apiService.js
@@ -0,0 +1,32 @@
+export default class NewsApiService {
+    constructor() {
+        
+        this.page = 1;
+        this.searchQuery = '';
+  }
+
+    fetchArticles(searchQuery) {
+      
+      return fetch(
+      `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=23546576-898e6f2f9578ee60189c27f38`,
+    )
+      .then(r => r.json())
+        .then(data => {
+            this.page += 1;
+            console.log(data);
+            return data.hits;
+      });
+    }
+    
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-import './sass/main.scss';
+import template from './template.hbs';
+import NewsApiService from './apiService';
+
+const searchForm = document.querySelector('.search-form');
+const loadMor = document.querySelector('.load-mor');
+const addGallery = document.querySelector('.container');
+const reset = document.querySelector('.reset');
+
+searchForm.addEventListener('submit', onSearch);
+loadMor.addEventListener('click', onloadMor);
+
+const newsApiService = new NewsApiService();
+
+function onSearch(e) {
+  e.preventDefault();
+
+  clearRequest();
+    newsApiService.query = e.currentTarget.elements.query.value;
+    newsApiService.resetPage();
+    newsApiService.fetchArticles().then(appMarkup);
+      
+}
+
+function onloadMor() {
+  newsApiService.fetchArticles().then(appMarkup); 
+}
+
+function appMarkup(hits) {
+  addGallery.insertAdjacentHTML('beforeend', template(hits));
+  setTimeout(() => {
+    addGallery.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end',
+    });
+  }, 1000)
+  reset.classList.remove('reset');
+}
+
+function clearRequest() {
+  addGallery.innerHTML = '';
+}
+
